72a7285e80237725b074ae458524d708b98d5526
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-08 16:17:56 +0200

    Update: parsed and stored the key-value pairs of envp

--- srcs/builtins/ft_env.c
+++ srcs/builtins/ft_env.c
@@ -1,10 +1,5 @@
 #include "minishell.h"
 
-
-///"srcs/includes/minishell.h"
-
-
-
 int env_size(char **env)
 {
 	int i;
@@ -15,81 +10,96 @@ int env_size(char **env)
 	return (i);
 }
 
-void	ft_env(char **env)
-{
-	int i;
-
-	i = 0;
-	while (env[i])
-	{
-		ft_putendl_fd(env[i], 1);
-		i++;
-	}
-}
 
 char *ft_strndup(char *src, size_t n)
 {
-    size_t i = 0;
-    char *dest = malloc(n + 1);  // Allocate memory (+1 for null terminator)
+    size_t i;
+    char *dest;
 
+	i = 0;
+	dest = malloc(sizeof(size_t) * (n + 1));
     if (!dest)
-        return NULL;
-
-    while (i < n && src[i] != '\0')  // Copy up to 'n' chars or until '\0'
+        return (NULL);
+    while (i < n && src[i] != '\0')
     {
         dest[i] = src[i];
         i++;
     }
-
-    dest[i] = '\0';  // Null-terminate the string
-    return dest;
+    dest[i] = '\0';
+    return (dest);
 }
+void 	 free_env(t_env *env, int size)
+{
+	int i;
 
+	i = 0;
+	while (i < size)
+	{
+		free(env[i].key);
+		free(env[i].value);
+		i++;
+	}
+	free(env);
+}
 
 t_env	*init_struct(char **envp)
 {
 	t_env 		*env_list;
-	int 		size;
 
-	size = env_size(envp);
-	env_list = malloc(sizeof(t_env) * (size + 1));
+	env_list = malloc(sizeof(t_env) * (env_size(envp) + 1));
 	if (!env_list)
 		return (NULL);
-	ft_memset(env_list, 0, sizeof(env_list) * size);
+	ft_memset(env_list, 0, sizeof(env_list) * env_size(envp));
 	return (env_list);
 }
 
-t_env	*init_env(char **envp)
+t_env	*init_env(char **envp, t_env *env)
 {
-	int i;
-	char *sign;
-	int key_len;
-	t_env 	*env;
-	int size;
+	char 		*sign;
+	int 		len;
+	int 		i;
 
-	size = env_size(envp);
 	env  = init_struct(envp);
 	if (!env)
 		return (NULL);
 	i = 0;
-	while (i <  size)
+	while (i < env_size(envp))
 	{
 		sign = ft_strchr(envp[i], '=');
-		if (sign)
+		if (!sign)
+			return (NULL);
+		len = sign - envp[i];
+		env[i].key = ft_strndup(envp[i], len);
+		env[i].value = ft_strdup(sign + 1);
+		if (!env[i].key || !env[i].value) 
 		{
-			key_len = sign - envp[i];
-			env[i].key = ft_strndup(envp[i], key_len);
-			env[i].value = ft_strdup(sign + 1);
-			if (!env[i].key || !env[i].value) 
-			{
-				free_env(env)
-				return (NULL);
-			}
+			free_env(env, env_size(envp));
+			return (NULL);
 		}
 		i++;
 	}
-	env[size].key = '\0';
-	env[size].value = '\0';
+	env[i].key = NULL;
+	env[i].value = NULL;
 	return (env);
 }
 
+void	ft_env(char **envp, t_env *env)
+{
+	int i;
+
+	i = 0;
+	env = init_env(envp, env);
+	if (!env)
+	{
+		printf("Error: initialize environment\n");
+		return ;
+	}
+	while (i < env_size(envp))
+	{
+		ft_putstr_fd(env[i].key, 1);
+		ft_putchar_fd('=', 1);
+		ft_putendl_fd(env[i].value, 1);
+		i++;
+	}
+	// free_env(env, env_size(envp));
+}
