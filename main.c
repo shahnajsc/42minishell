f5ab972d5c8de3aee7804ef83e97b0032895a3d9
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-08 16:19:23 +0200

    passed t_env 'array of struct' to ft_env()

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include "minishell.h"
 
 
-int main(int argc, char **argv, char **env)
+int main(int argc, char **argv, char **envp)
 {
+	t_env 	env;
+	char  *input;
 	(void)argc;
 	(void)argv;
-	char  *input;
 
 	while (1)
 	{
-		input = readline("minishell% ");
+		input = readline("minishell@42% ");
 		if (!input)
 			break;
 		if (ft_strcmp(input, "env") == 0)
-			ft_env(env);
-		if (ft_strcmp(input, "pwd") == 0)
+			ft_env(envp, &env);
+		else if (ft_strcmp(input, "pwd") == 0)
 			ft_pwd();
 		add_history(input);
 		free(input);
