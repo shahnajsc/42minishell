869a7ab6c3213bb9928729dc9ff45439aee2648a
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-10 17:55:09 +0200

    update

--- includes/minishell.h
+++ includes/minishell.h
@@ -57,6 +57,7 @@ void	minishell(t_mshell *mshell);
 int 	env_size(char **env);
 void	ft_env(t_env *env);
 int		ft_pwd(void);
+int 	ft_cd(t_env **env, char *path);
 char 	*ft_strndup(char *src, size_t n);
 void 	export_var(t_env **env, char *str);
 t_env	*duplicate_env(char **envp, t_env **env);
