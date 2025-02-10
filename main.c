b739c8b133e83f48106f8007da32f1a7d550152a
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-10 08:41:28 +0200

    Update: passed env by reference

--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	t_env 	env;
+	t_env 	*env;
 	char  *input;
 
 	(void)argc;
 	(void)argv;
+	duplicate_env(envp, &env);
 
 	while (1)
 	{
@@ -17,14 +18,15 @@ int main(int argc, char **argv, char **envp)
 		if (!input)
 			break;
 		if (ft_strcmp(input, "env") == 0)
-			ft_env(envp, &env);
+			ft_env(envp, env);
 		else if (ft_strcmp(input, "pwd") == 0)
 			ft_pwd();
 		else if (ft_strcmp(cmd, "export") == 0)
-			ft_export(&env, envp, split[1]);
+			ft_export(env, envp, split[1]);
 		add_history(input);
 		free(input);
 	}
 	rl_clear_history();
+	free_env(env, env_size(envp));
 	return (EXIT_SUCCESS);
 }
