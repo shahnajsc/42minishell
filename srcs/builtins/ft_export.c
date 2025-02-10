62c36384ca9b6e769b08343b47c25daa2fcc94c7
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-10 10:16:52 +0200

    Update: export command fixed. the variable appears both in env and export

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -1,24 +1,65 @@
 #include "minishell.h"
 
-
-void 	ft_export(t_env *env, char **envp, char *str)
+t_env 	*find_env_var(t_env *env, char *key)
 {
-	char 	*sign;
-	char 	*key;
-	char 	*value;
-	int 	size;
-	int 	len;
+	int		i;
+
+	i = 0;
+	while (env[i].key != NULL)
+	{
+		if (ft_strcmp(env[i].key, key) == 0)
+			return (&env[i]);
+		i++;
+	}
+	return (NULL);
+}
+void   add_env_var(t_env **env, char *key, char *value)
+{
 	int 	i;
-	// t_env 	*new_env;
+	int 	j;
+	t_env 	*new_env;
 
-	env = init_env(envp, env);
-
+	i = 0;
+	while ((*env)[i].key != NULL)
+		i++;
+	new_env = malloc(sizeof(t_env) * (i + 2));
+	if (!new_env)
+		return ;
+	j = 0;
+	while (j < i)
+	{
+		new_env[j].key = (*env)[j].key;
+		new_env[j].value = (*env)[j].value;
+		j++;
+	}
+	new_env[i].key = ft_strdup(key);
+    new_env[i].value = value ? ft_strdup(value) : NULL;
+    new_env[i + 1].key = NULL;
+    new_env[i + 1].value = NULL;
+	free(*env);
+	*env = new_env;
+	//   j = 0;
+    // while ((*env)[j].key != NULL)
+    // {
+    //     printf("%s=%s\n", (*env)[j].key, (*env)[j].value);
+    //     j++;
+    // }
+}
+void 	export_var(t_env **env, char *str)
+{
+	t_env 	*variable;
+	char 	*value;
+	char 	*key;
+	char 	*sign;
 
 	sign = ft_strchr(str, '=');
 	if (!sign)
+	{
+		if (!find_env_var(*env, str))
+			add_env_var(env, str, NULL);
 		return ;
-	len = sign - str;
-	key = ft_strndup(str, len);
+	}
+	key = ft_strndup(str, sign - str);
 	value = ft_strdup(sign + 1);
 	if (!key || !value)
 	{
@@ -26,51 +67,33 @@ void 	ft_export(t_env *env, char **envp, char *str)
 		free(value);
 		return ;
 	}
-	size = env_size(envp);
-	i = 0;
-	while (i < size)
+	variable = find_env_var(*env, key);
+	if (variable)
 	{
-		if (ft_strcmp(env[i].key, key) == 0)
+		free(variable->value);
+		variable->value = value;
+	}
+	else
+		add_env_var(env, key, value);
+	free(key);
+}
+void 	ft_export(t_env *env)
+{
+	int 	i;
+
+	i = 0;
+	while (env[i].key != NULL)
+	{
+		ft_putstr_fd("declare -x ", 1);
+		ft_putstr_fd(env[i].key, 1);
+		if (env[i].value)
 		{
-			free(env[i].value);
-			env[i].value = value;
-			free(key);
-			break ;
+			ft_putstr_fd("=\"", 1);
+			ft_putstr_fd(env[i].value, 1);
+			ft_putstr_fd("\"", 1);
 		}
+		ft_putstr_fd("\n", 1);
 		i++;
 	}
-	// new_env = malloc(sizeof(t_env) * (size + 2));
-	// if (!new_env)
-	// {
-	// 	free(key);
-	// 	free(value);
-	// 	return ;
-	// }
-	// i = 0;
-	// while (i < size)
-	// {
-	// 	new_env[i].key = env[i].key;
-	// 	new_env[i].value = env[i].value;
-	// 	i++;
-	// }
-	// new_env[i].key = key;
-	// new_env[i].value = value;
-	// new_env[i + 1].key = NULL;
-	// new_env[i + 1].value = NULL;
-	// free(env);
-	// env = new_env;
-// printf("%s=%s\n", new_env[0].key, new_env[0].value);
-	int j = 0;
-	
-	while (j < size)
-	{
-		printf("%s=%s\n", env[j].key, env[j].value);
-		j++;
-	}
-
-
-	// add_env_var(env, key, value);
-	// free(key);
-	free(value);
 }
 
