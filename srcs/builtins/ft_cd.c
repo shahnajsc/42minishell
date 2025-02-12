618ecb61430f7ab10053c74bac68e4b599382921
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-12 11:38:51 +0200

    Add: added a validity check for directory and handled the change to home directory when there is no path provided

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -22,35 +22,94 @@ void 	update_env(t_env **env, const char *key, const char *set_value)
 			}
 			free((*env)[j].value);
 			(*env)[j].value = temp;
+			// free(temp);
 			return ;
 		}
 		j++;
 	}
 }
 
+int 	check_directory(const char *path)
+{
+	struct stat	path_stat;
+
+	if (stat(path, &path_stat) != 0)
+	{
+		ft_putstr_fd("bash: ", 2);
+		ft_putstr_fd("cd: ", 2);
+		ft_putstr_fd((char *)path, 2);
+		ft_putstr_fd(": ", 2);
+		ft_putendl_fd("No such file or directory", 2);
+        return (0);
+	}
+	if (S_ISDIR(path_stat.st_mode))
+        return (1);
+	ft_putstr_fd("bash: ", 2);
+    ft_putstr_fd("cd: ", 2);
+    ft_putstr_fd((char *)path, 2);
+    ft_putstr_fd(": Not a directory", 2);
+    ft_putendl_fd("", 2);
+	return (0);
+}
+
+int 	home_directory(t_env **env, char **path)
+{
+	char 	*home;
+	int 	i;
+
+	if (!*path)
+	{
+		i = 0;
+		while ((*env)[i].key != NULL)
+		{
+			if (ft_strcmp((*env)[i].key, "HOME") == 0)
+				home = (*env)[i].value;
+			i++;
+		}
+		if (!home)
+			return (1);
+		*path = home;
+	}
+	return (0);
+}
+
+int change_directory(const char *path)
+{
+    if (chdir(path) != 0)
+    {
+        ft_putstr_fd("bash: cd: ", 2);
+        ft_putstr_fd((char *)path, 2);
+        ft_putstr_fd(": ", 2);
+        if (errno == ENOENT)   // Handle different errors based on errno
+            ft_putendl_fd("No such file or directory", 2);
+        else if (errno == EACCES)
+            ft_putendl_fd("Permission denied", 2);
+        else if (errno == ENOTDIR)
+            ft_putendl_fd("Not a directory", 2);
+        else
+            ft_putendl_fd(strerror(errno), 2); // Other errors
+        return (1);
+    }
+    return (0); 
+}
+
 int ft_cd(t_env **env, char *path)
 {
 	char 	*oldpwd;
 	char 	*newpwd;
-	char 	*home;
 
+	if (home_directory(env, &path))
+		return (1);
 	oldpwd = getcwd(NULL, 0);
 	if (!oldpwd)
 		return (1);
-	if (!path)
+	if (!check_directory(path))
 	{
-		home = getenv("HOME");
-		if (!home)
-		{
-			printf("cd: HOME not set\n");
-			free(oldpwd);
-			return (1);
-		}
-		path = home;
+		free(oldpwd);
+		return (1);
 	}
-	if (chdir(path))
+	if (change_directory(path))
 	{
-		printf("chdir error");
 		free(oldpwd);
 		return (1);
 	}
