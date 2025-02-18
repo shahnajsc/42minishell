c37874f906ae1fd2617ac2b5a4a91d7c2e40b755
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-18 14:57:39 +0200

    Updated

--- srcs/minishell/minishell_initiate.c
+++ srcs/minishell/minishell_initiate.c
@@ -1,22 +1,22 @@
-#include "minishell.h"
+// #include "minishell.h"
 
-void	minishell(t_mshell *mshell)
-{
-	char	*input_str;
+// void	minishell(t_mshell *mshell)
+// {
+// 	char	*input_str;
 
-	while (1) // need signal handle for exit
-	{
-		input_str = readline(PROMPT);
-		if (!input_str)
-		{
-			printf("no input\n");
-			break;
-		}
-		add_history(input_str);
-		free(input_str);
-	}
-	if (input_str)
-		mshell->input = input_str;
-	rl_clear_history();
-	return ;
-}
+// 	while (1) // need signal handle for exit
+// 	{
+// 		input_str = readline(PROMPT);
+// 		if (!input_str)
+// 		{
+// 			printf("no input\n");
+// 			break;
+// 		}
+// 		add_history(input_str);
+// 		free(input_str);
+// 	}
+// 	if (input_str)
+// 		mshell->input = input_str;
+// 	rl_clear_history();
+// 	return ;
+// }
