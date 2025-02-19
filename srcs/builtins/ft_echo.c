830708232fcd44ffefd1a5604c6681031e3bcc64
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-19 13:00:14 +0200

    Update: defined ft_echo but does not handle the -n flag

---
+++ srcs/builtins/ft_echo.c
@@ -0,0 +1,39 @@
+#include "minishell.h"
+
+
+void	ft_echo(t_env **env, char *str)
+{
+	int 	i;
+	int 	j;
+	char *key;
+
+	if (!str)
+		return ;
+	i = 0;
+	if (ft_strchr("$", str[i]))
+	{
+		j = 0;
+		while ((*env)[j].key != NULL)
+		{
+			key = str + 1;
+			if (ft_strcmp((*env)[j].key, key) == 0)
+			{
+				ft_putstr_fd((*env)[j].value, 1);
+				ft_putchar_fd('\n', 1);
+				return ;
+			}
+			j++;
+		}
+	}
+	if (str != NULL)
+		ft_putstr_fd(str, 1);
+	ft_putchar_fd('\n', 1);
+}
+
+// void 	ft_nl(char *str)
+// {
+// 	if (!str)
+// 		return ;
+// 	if (ft_strchr(*str, 'n'))
+// 		ft_putstr_fd(str, 1);
+// }
