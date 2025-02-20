ef3319e240bd7c39b88b96a6318c059ae5f402cb
Hager Hamdan <hahamdan@c1r3p7.hive.fi>
2025-02-20 18:04:17 +0200

    Update: added null check for env, key and if key is empty string

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -1,11 +1,13 @@
 #include <minishell.h>
 
-void 	update_env(t_env **env, const char *key, const char *set_value)
+void 	update_env(t_env **env, const char *key, const char *set_path)
 {
 	int 	i;
 	int 	j;
 	char 	*temp;
 
+	if (!env || !key || !*key || !set_path)
+		return ;
 	i = 0;
 	while ((*env)[i].key != NULL)
 		i++;
@@ -14,11 +16,11 @@ void 	update_env(t_env **env, const char *key, const char *set_value)
 	{
 		if (ft_strcmp((*env)[j].key, (char *)key) == 0)
 		{
-			temp = ft_strdup(set_value);
+			temp = ft_strdup(set_path);
 			if (!temp)
 			{
 				printf("allocation failed\n");
-				return ;
+				return (1);
 			}
 			free((*env)[j].value);
 			(*env)[j].value = temp;
@@ -38,37 +40,18 @@ void 	cd_error(const char *path, char *msg)
 }
 int 	check_permissions(const char *path)
 {
-	if (access(path, W_OK) != 0)
-	{	
-		cd_error(path, "Permission denied");
-		return (0);
-	}
-	else if (access(path, X_OK) != 0)
-	{	
-		cd_error(path, "Permission denied");
-		return (0);
-	}
-	else if (access(path, R_OK) != 0)
-	{	
-		cd_error(path, "Permission denied");
-		return (0);
-	}
-	return (1);
+	if (access(path, W_OK) != 0 || access(path, X_OK) != 0 || access(path, R_OK) != 0)
+		return (1);  // minishell: cd: test/: Permission denied
+	return (0);
 }
 int 	check_directory(const char *path)
 {
 	struct stat	path_stat;
 
 	if (stat(path, &path_stat) == -1)
-	{	
-		cd_error(path, "No such file or directory");
-		return (0);
-	}
+		return (1);   // minishell: cd: hello: No such file or directory
 	if (!S_ISDIR(path_stat.st_mode))
-	{
-		cd_error(path, "Not a directory");
-		return (0);
-	}
+		return (1);  // minishell: cd: includes/minishell.h: Not a directory
 	return (check_permissions(path));
 }
 
@@ -91,7 +74,7 @@ int 	home_directory(t_env **env, char **path)
 			i++;
 		}
 		if (!home)
-			return (1);
+			return (1);  // minishell: cd: HOME not set
 		*path = home;
 	}
 	return (0);
@@ -100,11 +83,20 @@ int 	home_directory(t_env **env, char **path)
 int change_directory(const char *path)
 {
     if (chdir(path) != 0)
-    {
         return (1);
-    }
     return (0); 
 }
+// int	builtin_err_print(t_mshell *mshell, char *err_msg, char *cmd)
+// {
+// 	ft_putstr_fd("minishell: ", 2);
+// 	if (err_msg)
+// 		ft_putstr_fd(err_msg, 2);
+// 	if (cmd)
+// 		ft_putstr_fd(cmd, 2);
+	
+// 	mshell->exit_code = 1;
+// 	return (1);
+// }
 
 int ft_cd(t_env **env, char *path)
 {
@@ -116,10 +108,12 @@ int ft_cd(t_env **env, char *path)
 	oldpwd = getcwd(NULL, 0);
 	if (!oldpwd)
 		return (1);
-	if (!check_directory(path))
+		
+	if (check_directory(path))
 	{
 		free(oldpwd);
 		return (1);
+		// return (builtin_err_print(mshell, "not a directory", path)); //after paring
 	}
 	if (chdir(path) != 0)
 	{
@@ -129,6 +123,7 @@ int ft_cd(t_env **env, char *path)
 	newpwd = getcwd(NULL, 0);
 	if (!newpwd)
 		return (1);
+	
 	update_env(env, "OLDPWD", oldpwd);
 	update_env(env, "PWD", newpwd);
 	free(oldpwd);
