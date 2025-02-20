bd9e6c293554276e7f029ba5061c56ef2d1fbb96
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-20 18:08:40 +0200

    Update: completed the unset command

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -2,24 +2,47 @@
 
 void	ft_unset(t_env **env, char *key)
 {
-	t_env 	*variable;
-	int 	size;
-	int 	i;
+	t_env	*variable;
+	// int		size;
+	int		i;
+	int j = 0;
+	// printf("%s\n", key);
+
+	if (!env || !*env || !key)
+		return;
 
 	variable = find_env_var(*env, key);
 	if (!variable)
-		return ;
-	size = env_size(env);
-	while (i < size)
+		return;
+
+	while ((*env)[j].key != NULL)
+		j++;
+	i = 0;
+	while (i < j)
 	{
 		if (&(*env)[i] == variable)
 		{
 			free((*env)[i].key);
 			free((*env)[i].value);
-			break ;
-		}		
+			
+			// Shift remaining elements left
+			while (i < j - 1)
+			{
+				(*env)[i] = (*env)[i + 1];
+				i++;
+			}
+
+			// Nullify the last element
+			(*env)[j - 1].key = NULL;
+			(*env)[j - 1].value = NULL;
+
+			return;
+		}
+		i++;
 	}
 }
+
+
 // 	if (variable)
 // 	{
 // 		free()
