a79f613a547d1f724f40daf285ea8c58aa5615c0
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-20 12:36:31 +0200

    Update: moved all the used functions/struct for the builtins from this main header file to another header file for more organized work

--- includes/minishell.h
+++ includes/minishell.h
@@ -10,6 +10,7 @@
 // header files
 # include "pipe.h"
 # include "envp.h"
+# include "builtins.h"
 # include "../libft/includes/libft.h"
 
 
@@ -46,28 +47,10 @@ typedef enum {
 	HERE_DOC
 }  token_type;
 
-typedef struct s_env {
-
-	char		*key;
-	char		*value;
-
-} t_env;
-
 
 
 
 //FUNCTIONS
-// void	minishell(t_mshell *mshell);
-int 	env_size(char **env);
-void	ft_env(t_env *env);
-int		ft_pwd(void);
-int 	ft_cd(t_env **env, char *path);
-char 	*ft_strndup(char *src, size_t n);
-void 	export_var(t_env **env, char *str);
-t_env	*duplicate_env(char **envp, t_env **env);
-void 	 free_env(t_env *env, int size);
-void 	ft_export(t_env *env);
-void 	export_var(t_env **env, char *str);
-void 	update_env(t_env **env, const char *key, const char *set_value);
+
 
 #endif
