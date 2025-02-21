ac8c1fc704b52fb16c526f9ddb1ddb202e335af4
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-21 17:36:08 +0200

    Upadte; created 'echo_pwd-oldpwd' function to echo/handle pwd and oldpwd paths internally so they don't rely on the environmnet. Also refactored ft_echo for readability

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -1,28 +1,42 @@
 #include "minishell.h"
 
-
-void	ft_echo(t_env **env, char *str)
+void		echo_pwd_oldpwd(t_env **env, t_shell_state *state, char *key)
 {
-	int 	i;
-	int 	j;
+	t_env	*variable;
+	char	*env_value;
+
+	variable = find_env_var(*env, key);
+	env_value = variable ? variable->value : NULL;
+	if (env_value)
+		ft_putstr_fd(env_value, STDOUT_FILENO);
+	else if (state->cd_executed)
+	{
+		if (ft_strcmp(key, "OLDPWD") == 0 && state->old_pwd)
+			ft_putstr_fd(state->old_pwd, STDOUT_FILENO);
+		else if (ft_strcmp(key, "PWD") == 0 && state->pwd)
+			ft_putstr_fd(state->pwd, STDOUT_FILENO);
+	}
+	// ft_putstr_fd("\n", STDOUT_FILENO);
+}
+
+
+void	ft_echo(t_env **env, t_shell_state *state, char *str)
+{
 	char 	*key;
+	t_env 	*env_var;
 
 	if (!str)
 		return ;
-	i = 0;
-	if (ft_strchr("$", str[i]))
+	if (ft_strchr("$", *str))
 	{
-		j = 0;
-		while ((*env)[j].key != NULL)
+		key = str + 1;
+		if (ft_strcmp(key, "PWD") == 0 || ft_strcmp(key, "OLDPWD") == 0 )
+			echo_pwd_oldpwd(env, state, key);
+		else
 		{
-			key = str + 1;
-			if (ft_strcmp((*env)[j].key, key) == 0)
-			{
-				ft_putstr_fd((*env)[j].value, 1);
-				ft_putchar_fd('\n', 1);
-				return ;
-			}
-			j++;
+			env_var = find_env_var(*env, key);
+			if (env_var->value)
+				ft_putstr_fd(env_var->value, STDOUT_FILENO);
 		}
 		ft_putchar_fd('\n', 1);
 		return ;
@@ -31,6 +45,7 @@ void	ft_echo(t_env **env, char *str)
 		ft_putstr_fd(str, 1);
 	ft_putchar_fd('\n', 1);
 }
+
 
 // void 	ft_nl(char *str)
 // {
