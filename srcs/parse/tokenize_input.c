329e401c2dce7394a98c48e4d4b222ed69d65e80
shahnajsc <sschowdhury29@gmail.com>
2025-02-24 19:16:58 +0200

    update: parse: tokenization

--- srcs/parse/tokenize_input.c
+++ srcs/parse/tokenize_input.c
@@ -1,49 +1,46 @@
 #include "minishell.h"
 
+// static t_token	init_token(t_mshell *mshell, t_cmd *cmd_ptr)
+// {
+// 	mshell->cmds->token = (t_token *)ft_calloc(2, sizeof(t_token));
+// 	if (!mshell->cmds->token)
+// 		return (NULL);
+// }
+
 static void	init_cmds(t_mshell*mshell, char *input_str)
 {
 	char	**cmds_temp;
 	int	i;
 
 	i = 0;
-	printf("token function begin \n");
 	cmds_temp = split_input_by_pipes(mshell->count_cmds, input_str);
 	if (!cmds_temp)
 	{
 		printf("no splitted cmds token function\n");
 		return ;
 	}
-	// while (cmds_temp[i])
-	// {
-	// 	printf("a[%d] : value[%s]\n", i, cmds_temp[i]);
-	// 	i++;
-	// }
-	// printf("\n");
-	// if (!cmds_temp)
-	// 	return ;
-	while (i < mshell->count_cmds + 1)
+	while (cmds_temp[i])
 	{
-		printf("ms[%d] : value[%s]\n", i, cmds_temp[i]);
 		mshell->cmds[i].cmd = cmds_temp[i];
 		mshell->cmds[i].in_fd= -1;
 		mshell->cmds[i].out_fd= -1;
-		mshell->cmds[i].splitted_cmd = NULL; //make splitted cmd
+		mshell->cmds[i].splitted_cmd = ft_split(mshell->cmds[i].cmd, ' '); //make splitted cmd
+		if (!mshell->cmds[i].splitted_cmd)
+			return ;
 		mshell->cmds[i].token = NULL; // make token
 		i++;
 	}
+	printf("\n");
 }
 
 static int	update_mshell(t_mshell*mshell, char *input_str)
 {
 	if (!mshell)
 		return (1);
-	mshell->count_cmds = count_pipes(input_str);
+	mshell->count_cmds = count_pipes(input_str) + 1;
 	mshell->cmds = (t_cmd *)ft_calloc(mshell->count_cmds, sizeof(t_cmd));
 	if  (!mshell->cmds)
 		return (1);
-	// mshell->cmds->token = (t_token *)ft_calloc(mshell->token_list->total_token, sizeof(t_token));
-	// if (!mshell->token_list->token)
-	// 	return (1);
 	init_cmds(mshell, input_str);
 	return (0);
 }
@@ -54,12 +51,36 @@ int	tokenize_input(t_mshell *mshell, char *input_str)
 		return (syntax_pre_error(mshell, ERR_COMN, "Failed: tokenize_init()_1")); //check for actual  bash error code
 	if (update_mshell(mshell, input_str))
 		return (syntax_pre_error(mshell, ERR_COMN, "Failed: tokenize_init()_2")); //check for actual  bash error code
-	//printf("id[%d] : value[%s]\n", 1, mshell->cmds[0].cmd);
-	//int i = 0;
-	// while (mshell->cmds->cmd)
-	// {
-	// 	printf("id[%d] : value[%s]\n", 1, mshell->cmds[0].cmd);
-	// 	//i++;
-	// }
 	return (0);
 }
+//................................
+	// printf("full str:[%s]\n", input_str);
+	// int k = 0;
+	// while (cmds_temp[k] != NULL)
+	// {
+	// 	printf("b[%d] : value[%s]\n", k, cmds_temp[k]);
+	// 	k++;
+	// }
+	// printf("\n");
+
+	//................................
+		//printf("id[%d] : value[%s]\n", 1, mshell->cmds[0].cmd);
+	// int i = 0;
+	// while (i < mshell->count_cmds)
+	// {
+	// 	printf("id[%d] : value[%s]\n", 1, mshell->cmds[i].cmd);
+	// 	i++;
+	// }
+
+		//printf("inside struct:[%d] : value[%s] ", i, mshell->cmds[i].cmd);
+		// printf("in_id:[%d]", mshell->cmds[i].in_fd);
+		// printf("out_id:[%d]\n", mshell->cmds[i].out_fd);
+
+	// int k = 0;
+	// //int m = 0;
+	// while (mshell->cmds[i].splitted_cmd[k] != NULL)
+	// {
+	// 	printf("cmd[%d] sp[%d] : value[%s]\n", i, k, mshell->cmds[i].splitted_cmd[k]);
+	// 	k++;
+	// }
+	// printf("\n");
