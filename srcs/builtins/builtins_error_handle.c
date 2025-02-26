a158051e005b8d797894f0ce90a506c78fa09a0d
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-26 15:12:43 +0200

    Update: added an error messege

--- srcs/builtins/builtins_error_handle.c
+++ srcs/builtins/builtins_error_handle.c
@@ -1,11 +1,12 @@
 #include "minishell.h"
 
-int 	builtins_error(char *args, t_error err, char *msg, char *free_str)
+int 	builtins_error(char **args, t_error err, char *msg, char *free_str)
 {
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
-	ft_putstr_fd("cd: ", STDERR_FILENO);
-	if (args[1])
-		ft_putstr_fd(args, STDERR_FILENO);
+	ft_putstr_fd(args[0], STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	if (args[1] && !args[2])  // check this
+		ft_putstr_fd(args[1], STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
 	if (err == NO_FILE)
 		ft_putendl_fd("No such file or directory", STDERR_FILENO);
@@ -13,8 +14,10 @@ int 	builtins_error(char *args, t_error err, char *msg, char *free_str)
 		ft_putendl_fd("Not a directory", STDERR_FILENO);
 	if (err == NO_PERM)
 		ft_putendl_fd("Permission denied", STDERR_FILENO);
-	if (err == UNSET_HOME)
+	if (err == HOME_UNSET)
 		ft_putendl_fd("HOME not set", STDERR_FILENO);
+	if (err == TOO_MANY_ARGS)
+		ft_putendl_fd("too many arguments", STDERR_FILENO);
 	else
 		ft_putendl_fd(msg, STDERR_FILENO);
 	if (free_str)
