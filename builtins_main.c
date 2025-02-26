4e59311b871522f39db2103f1c641e723d4a3bdc
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-26 15:15:55 +0200

    initiating the needed data

--- builtins_main.c
+++ builtins_main.c
@@ -1,65 +1,30 @@
 #include "minishell.h"
 
-
-int main(int argc, char **argv, char **envp)
+static int mshell_data_init(t_mshell *mshell, char **envp)
 {
-	t_env 	*env;
-	char  *input;
-
-	(void)argc;
-	(void)argv;
-	duplicate_env(envp, &env);
-
-	while (1)
-	{
-		input = readline("minishell@42% ");
-		// printf("%s\n", input);
-		char **split = ft_split(input, ' ');
-		char *cmd = split[0];
-		if (!input)
-			break;
-		if (ft_strcmp(input, "env") == 0)
-			ft_env(env);
-		else if (ft_strcmp(input, "pwd") == 0)
-			ft_pwd();
-		else if (ft_strcmp(cmd, "export") == 0)
-		{
-			if (split[1])
-				export_var(&env, split[1]);
-			else
-				ft_export(env);
-		}
-		else if (ft_strcmp(cmd, "cd") == 0)
-			ft_cd(&env, split[1]);
-		else if (ft_strcmp(cmd, "echo") == 0)
-			ft_echo(&env, split[1]);
-		else if (ft_strcmp(cmd, "unset") == 0)
-			ft_unset(&env, split[1]);
-		add_history(input);
-		free(input);
-	}
-	rl_clear_history();
-	free_env(env, env_size(envp));
-	return (EXIT_SUCCESS);
+	ft_memset(mshell, 0, sizeof(mshell));
+	mshell->env = duplicate_env(&mshell->env, envp); //change your one
+	if (!mshell->env)
+		return (1);
+	mshell->b_state = init_built_state();
+	if (!mshell->b_state)
+		return (1);
+	return (0);
 }
-// int main(int argc, char **argv)
-// {
-// 	char	*input_str;
 
-// 	(void)argc;
-// 	(void)argv;
-// 	while (1) // need signal handle for exit
-// 	{
-// 		input_str = readline(PROMPT);
-// 		if (!input_str)
-// 		{
-// 			printf("no input\n");
-// 			break;
-// 		}
-// 		add_history(input_str);
-// 		free(input_str);
-// 	}
-// 	if (input_str)
-// 	rl_clear_history();
-// 	return (EXIT_SUCCESS);
-// }
+
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_mshell mshell;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_putstr_fd("Error: Correct  argument: ./minishell\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	if (mshell_data_init(&mshell, envp))
+		exit(EXIT_FAILURE);
+	minishell(&mshell);
+}
