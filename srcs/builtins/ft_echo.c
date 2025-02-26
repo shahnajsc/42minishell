0cb3329c185cc1f6816ec51043845afdf2182e08
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-26 14:45:38 +0200

    Uppate; Refactored 'ft_echo' so it does not handle the expansion part of echo '$'. the function currently handles mutliple arguments and the flag '-n' to remove new line.

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -1,57 +1,50 @@
 #include "minishell.h"
 
-void		echo_pwd_oldpwd(t_env **env, t_shell_state *state, char *key)
+void		echo_pwd_oldpwd(t_mshell *mshell, char *key)
 {
 	t_env	*variable;
 	char	*env_value;
 
-	variable = find_env_var(*env, key);
+	variable = find_env_var(mshell->env, key);
 	env_value = variable ? variable->value : NULL;
 	if (env_value)
 		ft_putstr_fd(env_value, STDOUT_FILENO);
-	if (state->oldpwd_exec && ft_strcmp(key, "OLDPWD") == 0 && state->old_pwd)
-			ft_putstr_fd(state->old_pwd, STDOUT_FILENO);
-	if (state->pwd_exec && ft_strcmp(key, "PWD") == 0 && state->pwd)
-			ft_putstr_fd(state->pwd, STDOUT_FILENO);
+	if (mshell->b_state->oldpwd_exec && ft_strcmp(key, "OLDPWD") == 0 && mshell->b_state->old_pwd)
+			ft_putstr_fd(mshell->b_state->old_pwd, STDOUT_FILENO);
+	if (mshell->b_state->pwd_exec && ft_strcmp(key, "PWD") == 0 && mshell->b_state->pwd)
+			ft_putstr_fd(mshell->b_state->pwd, STDOUT_FILENO);
 }
-
-
-void	ft_echo(t_env **env, t_shell_state *state, char *str)
+int	is_newline(char *c, int *nl)
 {
-	char 	*key;
-	t_env 	*env_var;
-
-	if (!str)
-		return ;
-	if (ft_strchr("$", *str))
+	if (ft_strcmp(c, "-n") == 0)
 	{
-		key = str + 1;
-		if (ft_strcmp(key, "PWD") == 0 || ft_strcmp(key, "OLDPWD") == 0 )
-			echo_pwd_oldpwd(env, state, key);
-		else
-		{
-			env_var = find_env_var(*env, key);
-			if (env_var->value)
-				ft_putstr_fd(env_var->value, STDOUT_FILENO);
-		}
-		ft_putchar_fd('\n', 1);
-		return ;
+		*nl= 0;  
+		return (1);  
 	}
-	if (str != NULL)
-		ft_putstr_fd(str, 1);
-	ft_putchar_fd('\n', 1);
+	*nl = 1;  
+	return (0);
 }
+void	ft_echo(t_mshell *mshell, char **args)
+{
+	int 		i;
+	int 		new_line;
+	t_env 		*variable;
 
-
-// void 	ft_nl(char *str)
-// {
-// 	if (!str)
-// 		return ;
-// 	if (ft_strchr(*str, 'n'))
-// 		ft_putstr_fd(str, 1);
-// }
-//echo $home
-//echo /haamda/abc/
-
-//echo -n $abc
-//echo -n 
+	i = 1;
+	new_line = 1;
+	if (args[i] && is_newline(args[i], &new_line))
+		i++;
+	while (args[i] != NULL)
+	{
+		variable = find_env_var(mshell->env, args[i]);
+		if (variable && variable->value)
+			ft_putstr_fd(variable->value, STDOUT_FILENO);
+		else
+			ft_putstr_fd(args[i], STDOUT_FILENO);
+		if (args[i + 1] != NULL)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+	if (new_line)
+		ft_putchar_fd('\n', STDOUT_FILENO);	
+}
