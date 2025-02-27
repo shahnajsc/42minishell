4e016fd027e1e07c141309d1eb6c40b84f6fa0f8
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-27 17:50:54 +0200

    moved init_built_state hell_state because it is not being used for now

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -1,5 +1,54 @@
 #include <minishell.h>
 
+t_built_state 	*init_built_state()
+{
+	t_built_state *state;
+
+	state = (t_built_state *)malloc(sizeof(t_built_state));
+	if (!state)
+	{
+		ft_putendl_fd("minishell: shell state", STDERR_FILENO);
+		return (NULL);
+	}
+	state->pwd = getcwd(NULL, 0);
+	if (!state->pwd)
+	{
+		free(state->pwd);
+		ft_putendl_fd("minishell: getcwd failed", STDERR_FILENO);
+		return (NULL);
+	}
+	state->old_pwd = NULL;
+	state->pwd_exec = 0;
+	state->oldpwd_exec = 0;
+	return (state);
+}
+
+int shell_state(t_mshell *mshell, char *oldpwd, char *pwd)
+{
+	char *new_oldpwd;
+	char *new_pwd;
+
+	new_oldpwd = ft_strdup(oldpwd);
+	new_pwd = ft_strdup(pwd);
+	if (!new_oldpwd || !new_pwd)
+	{
+		free(new_oldpwd);
+		free(new_pwd);
+		mshell->b_state->pwd = getcwd(NULL, 0);
+		mshell->b_state->old_pwd = NULL;
+		if (!mshell->b_state->pwd)
+			return(builtins_error(NULL, CMN_ERR, "minishell: getcwd failed for state->pwd", NULL));
+		//ft_putendl_fd("minishell: shell state allocation failed", STDERR_FILENO);
+	}
+	free(mshell->b_state->old_pwd);
+	free(mshell->b_state->pwd);
+	mshell->b_state->old_pwd = new_oldpwd;
+	mshell->b_state->pwd = new_pwd;
+	mshell->b_state->pwd_exec = (find_env_var(mshell->env, "PWD") == NULL);
+	mshell->b_state->oldpwd_exec = (find_env_var(mshell->env, "OLDPWD") == NULL);
+	return (SUCSSES);
+}
+
 void	update_cd_env(t_env **env, const char *key, const char *set_path)
 {
 	t_env	*variable;
@@ -49,55 +98,6 @@ int 	validate_directory(t_env **env, char **args)
 	return (SUCSSES);
 }
 
-
-t_built_state 	*init_built_state()
-{
-	t_built_state *state;
-
-	state = (t_built_state *)malloc(sizeof(t_built_state));
-	if (!state)
-	{
-		ft_putendl_fd("minishell: shell state", STDERR_FILENO);
-		return (NULL);
-	}
-	state->pwd = getcwd(NULL, 0);
-	if (!state->pwd)
-	{
-		free(state->pwd);
-		ft_putendl_fd("minishell: getcwd failed", STDERR_FILENO);
-		return (NULL);
-	}
-	state->old_pwd = NULL;
-	state->pwd_exec = 0;
-	state->oldpwd_exec = 0;
-	return (state);
-}
-
-int shell_state(t_mshell *mshell, char *oldpwd, char *pwd)
-{
-	char *new_oldpwd;
-	char *new_pwd;
-
-	new_oldpwd = ft_strdup(oldpwd);
-	new_pwd = ft_strdup(pwd);
-	if (!new_oldpwd || !new_pwd)
-	{
-		free(new_oldpwd);
-		free(new_pwd);
-		mshell->b_state->pwd = getcwd(NULL, 0);
-		mshell->b_state->old_pwd = NULL;
-		if (!mshell->b_state->pwd)
-			return(builtins_error(NULL, CMN_ERR, "minishell: getcwd failed for state->pwd", NULL));
-		//ft_putendl_fd("minishell: shell state allocation failed", STDERR_FILENO);
-	}
-	free(mshell->b_state->old_pwd);
-	free(mshell->b_state->pwd);
-	mshell->b_state->old_pwd = new_oldpwd;
-	mshell->b_state->pwd = new_pwd;
-	mshell->b_state->pwd_exec = (find_env_var(mshell->env, "PWD") == NULL);
-	mshell->b_state->oldpwd_exec = (find_env_var(mshell->env, "OLDPWD") == NULL);
-	return (SUCSSES);
-}
 void	update_built_state(t_mshell *mshell, char *oldpwd, char *pwd)
 {
 	shell_state(mshell, oldpwd, pwd);
@@ -113,8 +113,6 @@ int ft_cd(t_mshell *mshell, char **args)
 	char 			*pwd;
 	t_error 		check;
 
-	// if (!*args[1])         // check when the argumnet is empty >> cd ""
-	// 	return (0);
 	if (args[1] && args[2])
 		return (builtins_error(args, TOO_MANY_ARGS, NULL, NULL));
 	oldpwd = getcwd(NULL, 0);
