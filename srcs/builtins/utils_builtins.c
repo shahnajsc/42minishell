237d96f34a03a343c4e6a8c797fa8fbbd26c7ec5
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-02-28 15:27:16 +0200

    added a helper function

---
+++ srcs/builtins/utils_builtins.c
@@ -0,0 +1,35 @@
+#include "minishell.h"
+
+char *ft_strndup(char *src, size_t n)
+{
+    size_t i;
+    char *dest;
+
+	i = 0;
+	dest = malloc(sizeof(size_t) * (n + 1));
+    if (!dest)
+        return (NULL);
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+t_env 	*find_env_var(t_env *env, char *key)
+{
+	int		i;
+
+	if (!env || !key || !*key)
+		return (NULL);
+	i = 0;
+	while (env[i].key != NULL)
+	{
+		if (ft_strcmp(env[i].key, key) == 0)
+			return (&env[i]);
+		i++;
+	}
+	return (NULL);
+}
+
