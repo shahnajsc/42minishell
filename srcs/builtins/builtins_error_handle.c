10dca2d5d5f71f19cd2ca6e1a120c5905e0c1e04
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-01 11:52:18 +0200

    Update: defined an error handling function for all builtins

--- srcs/builtins/builtins_error_handle.c
+++ srcs/builtins/builtins_error_handle.c
@@ -1,12 +1,10 @@
 #include "minishell.h"
 
-int 	builtins_error(char **args, t_error err, char *msg, char *free_str)
+int cd_error(char **args, t_cd_error err)
 {
-	ft_putstr_fd("minishell: ", STDERR_FILENO);
-	ft_putstr_fd(args[0], STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
-	if (args[1] && !args[2])  // check this
-		ft_putstr_fd(args[1], STDERR_FILENO);
+	ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+	// if (args[1] && !args[2])    // needed?
+	ft_putstr_fd(args[1], STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
 	if (err == NO_FILE)
 		ft_putendl_fd("No such file or directory", STDERR_FILENO);
@@ -18,10 +16,15 @@ int 	builtins_error(char **args, t_error err, char *msg, char *free_str)
 		ft_putendl_fd("HOME not set", STDERR_FILENO);
 	if (err == TOO_MANY_ARGS)
 		ft_putendl_fd("too many arguments", STDERR_FILENO);
-	else
-		ft_putendl_fd(msg, STDERR_FILENO);
+	return (1);
+}
+int 	builtins_error(char *err_msg, char *free_str)
+{
 	if (free_str)
 		free(free_str);
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(err_msg, STDERR_FILENO);
+	ft_putchar_fd('\n', STDERR_FILENO);
 	return (1);
 }
 int 	is_invalid_identifier(char *identifier, char *arg)
