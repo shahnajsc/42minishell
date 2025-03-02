17cb32a5b2abfb7fda8447eb385ca657a32938fc
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-02 20:28:49 +0200

    Refactored: splitted the original 'add_env_var' into smaller mangable functions for better clarity and maintainig

--- srcs/builtins/utils_export.c
+++ srcs/builtins/utils_export.c
@@ -1,38 +1,50 @@
 #include "minishell.h"
 
-void   add_env_var(t_env **env, char *key, char *value)
+int copy_env(t_env *old_env, t_env *new_env)
 {
-	int 	i;
-	int 	j;
-	t_env 	*new_env;
+    int 	i;
 
 	i = 0;
-	while ((*env)[i].key != NULL)
-		i++;
-	new_env = malloc(sizeof(t_env) * (i + 2));
-	if (!new_env)
-		return ;
-	j = 0;
-	while (j < i)
-	{
-		new_env[j].key = ft_strdup((*env)[j].key);
-		new_env[j].value = (*env)[j].value ? ft_strdup((*env)[j].value) : NULL;
-		if (!new_env[j].key || (!new_env[j].value && (*env)[j].value))
-		{	
-			free_env(new_env, j);
-			printf("Error: allocation faild\n");
-			return ;
+    while (old_env[i].key != NULL) 
+    {
+        new_env[i].key = ft_strdup(old_env[i].key);
+		if (old_env[i].value)
+            new_env[i].value = ft_strdup(old_env[i].value);
+		else
+			new_env[i].value = NULL;
+        if (!new_env[i].key || (!new_env[i].value && old_env[i].value))
+		{
+			free_env(new_env);
+			free(new_env[i].value);
+			return(builtins_error("Allocation failed for new env", new_env[i].key));
 		}
-		j++;
-	}
-	new_env[i].key = ft_strdup(key);
-    new_env[i].value = value ? ft_strdup(value) : NULL;
-    new_env[i + 1].key = NULL;
-    new_env[i + 1].value = NULL;
-	free(*env);
-	*env = new_env;
+        i++;
+    }
+	return (0);
 }
 
+void   add_env_var(t_env **old_env, char *key, char *value)
+{
+	t_env 	*new_env;
+	int 	i;
+	
+	new_env = NULL;
+	new_env = allocate_new_env(*old_env);
+	if (!new_env)
+		return ;
+	if (copy_env((*old_env), new_env))
+		return ;
+	i = env_size(new_env);
+	new_env[i].key = ft_strdup(key);
+	if (value)
+		new_env[i].value = ft_strdup(value);
+	else
+		new_env[i].value = NULL;
+    new_env[i + 1].key = NULL;
+    new_env[i + 1].value = NULL;
+	free(*old_env);
+	*old_env = new_env;
+}
 
 void 	process_without_sign(t_env **env, char *arg)
 {
