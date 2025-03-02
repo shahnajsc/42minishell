d1c8ebb0c05c7334842ce241ff58535ce1e90882
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-02 18:25:07 +0200

    Defined 'mshell_level' to mange sell level (SHLVL in environment. the function retrevice, increment, and manage the initilization of 'SHLVL' environment variable if it does not exist. call function 'mshell_lvl_error to handle the case where SHLVL exeeds the limist (999 nested shells), causing the programe to terminate. memory leaks are handled as well.

--- srcs/builtins/env_init.c
+++ srcs/builtins/env_init.c
@@ -1,11 +1,11 @@
 #include "minishell.h"
 
-static int env_size(char **env)
+static int envp_size(char **envp)
 {
 	int i;
 
 	i = 0;
-	while (env[i])
+	while (envp[i])
 		i++;
 	return (i);
 }
@@ -32,44 +32,72 @@ static char *set_key_value(t_env *env, char *envp, char **sign)
 	return (envp);
 }
 
-static t_env	*duplicate_env(t_env **env, char **envp)
+int	duplicate_env(t_env **env, char **envp)
 {
 	char	*sign;
 	int		i;
 
 	i = -1;
-	while (++i < env_size(envp))
+	while (++i < envp_size(envp))
 	{
 		sign = ft_strchr(envp[i], '=');
 		if (!set_key_value(&((*env)[i]), envp[i], &sign))
-        {
-            free_env(*env, env_size(envp));
-            return (NULL);
-        }
+		{    
+			free_env(*env);
+			return(-1);
+		}
 	}
 	(*env)[i].key = NULL;
 	(*env)[i].value = NULL;
-	return (*env);
+	return (0);
 }
 
+int 	mshell_level(t_env **env)
+{
+	t_env 		*shlvl;
+	char 		*new_lvl;
+	int 		nbr;
+
+	shlvl = get_env_var(*env, "SHLVL");
+	if (!shlvl || !shlvl->value)
+		add_env_var(env, "SHLVL", "1");
+	else 
+	{
+		nbr = ft_atoi(shlvl->value);
+		new_lvl = ft_itoa(nbr + 1);
+		if (nbr < 999)
+		{
+			free(shlvl->value);
+			shlvl->value = new_lvl;
+		}
+		else
+			mshell_lvl_error(env, new_lvl);
+		//program should terminate
+	}
+	return (0);
+}
 t_env	*init_env(char **envp)
 {
 	t_env 		*env_list;
 	int 		size;
 
-	size = env_size(envp);
+	size = envp_size(envp);
 	env_list = malloc(sizeof(t_env) * (size + 1));
 	if (!env_list)
 	{
 		ft_putendl_fd("minishell: Allocation faild for env", STDERR_FILENO);
-		free(env_list);
 		return(NULL);
 	}
 	ft_memset(env_list, 0, sizeof(t_env) * (size + 1));
-	if (!duplicate_env(&env_list, envp))
+	if (duplicate_env(&env_list, envp) != 0)
 	{
 		free(env_list);
 		return (NULL);
+	}
+	if (mshell_level(&env_list))
+	{
+		free(env_list);
+		return(NULL);
 	}
 	return (env_list);
 }
