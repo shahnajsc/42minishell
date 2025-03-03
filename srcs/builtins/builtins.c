5dd6d2ef552ee05fc037604122623a0caf1e7833
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-03 20:08:26 +0200

    Update: Added 'ft_exit' function that mimics the behaviour of 'exit' command in bash

--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -8,9 +8,8 @@ int builtins_execv(t_mshell *mshell, char *input_str)
 	args = ft_split(input_str, ' ');
 	if (!args)
 		return (1);
-
 	if (ft_strcmp(args[0], "env") == 0)
-		ft_env(mshell->env);
+		ft_env(mshell, args);
 	else if (ft_strcmp(args[0], "pwd") == 0)
 		ft_pwd();
 	else if (ft_strcmp(args[0], "export") == 0)
@@ -19,8 +18,10 @@ int builtins_execv(t_mshell *mshell, char *input_str)
 		ft_cd(mshell, args);
 	else if (ft_strcmp(args[0], "echo") == 0)
 		ft_echo(mshell, args);
-	else if (ft_strcmp(args[0], "unset") == 0)
-		ft_unset(&mshell->env, args[1]);
+	else if (ft_strcmp(args[0], "exit") == 0)
+		ft_exit(mshell, args);
+	// else if (ft_strcmp(args[0], "unset") == 0)
+	// 	ft_unset(&mshell->env, args[1]);
 	add_history(input_str);
 	rl_clear_history();
 	// free_env(mshell->env, env_size(&mshell->env));
