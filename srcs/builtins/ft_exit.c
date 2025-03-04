d738816e8895518c1fd66c425313b2243b7ce924
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-04 15:37:02 +0200

    fixed: fixed exit code handling for large numbers in process_exit_code.                        defined 'ft_atoll' that handles 'long long' integer datatype to properly process exit code argument

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -1,60 +1,67 @@
 #include "minishell.h"
 
-int 	is_invalid_digit(char *arg)
+long long ft_atoll(const char *str, long long number, long long check)
 {
-	int 	i;
+    int signcount;
 
-	i = 0;
-	if (arg[i] == '-' || arg[i] == '+') 
-        i++;
-	while (arg[i] != '\0')
-	{
-		if (!ft_isdigit(arg[i]))
-			return (1);
-		i++;
-	}
+	signcount = 1;
+    if (*str == '-' || *str == '+')
+    {
+        if (*str == '-')
+            signcount = -1;
+        str++;
+    }
+    while (*str >= '0' && *str <= '9')
+    {
+        check = (number * 10) + (*str - '0');
+        if (check / 10 != number && signcount == 1)
+            return (LLONG_MAX);
+        if (check / 10 != number && signcount == -1)
+            return (LLONG_MIN);
+        number = check;
+        str++;
+    }
+    return (number * signcount);
+}
+
+int 	process_exit_code(char *arg, int *exit_status)
+{
+	long long 	exit_nbr;
+
+	exit_nbr = ft_atoll(arg, 0, 0);
+	if (exit_nbr == LLONG_MAX || exit_nbr == LLONG_MIN)
+		return (1);
+	else if (exit_nbr < 0)
+		exit_nbr = (exit_nbr % 256 + 256) % 256;
+	else if (exit_nbr > 255)
+		exit_nbr = exit_nbr % 256;
+	*exit_status = (int)exit_nbr;
 	return (0);
 }
-void 	process_exit_code(char *arg, int *exit_status)
-{
-	int 	nbr;
-
-	nbr = 0;
-	nbr = ft_atoi(arg);
-	if (nbr < 0)
-		nbr = (nbr % 256 + 256) % 256;
-	else if (nbr > 255)
-		nbr = nbr % 256;
-	*exit_status = nbr;
-}
-void 	handle_exit(char **args, int *exit_status)
+int 	handle_exit(char **args, int *exit_status)
 {	
 	if (!args[1])
 		*exit_status = 0;
 	else if (!is_invalid_digit(args[1]) && args[2])
 	{
-		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-		ft_putendl_fd("too many arguments", STDERR_FILENO);
+		builtins_error(args[0], "too many arguments", NULL);
 		*exit_status = 1;
 	}
-	else if (is_invalid_digit(args[1]))
+	else if (is_invalid_digit(args[1]) || process_exit_code(args[1], exit_status))
 	{
-		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-		ft_putstr_fd(args[1], STDERR_FILENO);
-		ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+		builtins_error(args[0], "numeric argument required", NULL);
 		*exit_status = 2;
 	}
-	else if (args[1])
-		process_exit_code(args[1], exit_status);
+	return (*exit_status);
 }
-int 	exit_mshell(t_mshell *mshell, int  *exit_status)
+int 	exit_mshell(t_mshell *mshell, int *exit_status)
 {
 	free_env(mshell->env);
 	exit (*exit_status);
 }
 int  ft_exit(t_mshell *mshell, char **args)
 {
-	int 	exit_status;
+	int	exit_status;
 
 	exit_status = 0;
 	ft_putendl_fd("exit", STDOUT_FILENO); 
