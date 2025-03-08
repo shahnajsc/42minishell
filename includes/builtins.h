95fa31789a51110d18d4f229fd81dfbedc52a5da
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-08 14:55:33 +0200

    Update

--- includes/builtins.h
+++ includes/builtins.h
@@ -18,27 +18,29 @@ typedef struct s_built_state {
 typedef enum s_cd_error
 {
 	SUCSSES,
+	FAILURE,
 	NO_FILE,
 	NOT_DIR,
 	NO_PERM,
 	HOME_UNSET,
 	CMN_ERR,
-	TOO_MANY_ARGS,
+	MANY_ARGS,
+	GETCWD,
 } t_cd_error;
 
 
 int				ft_env(t_mshell *mshell, char **args);
-int				ft_pwd(void);
+int				ft_pwd(t_mshell *mshell);
 int 			builtins_error(char *arg, char *err_msg, char *free_str);
 int 			ft_cd(t_mshell *mshell, char **args);
 char 			*ft_strndup(char *src, size_t n);
 void			set_env_variable(t_mshell *mshell, char **args, int *err_code);
 void 			free_env(t_env *env);
 void			ft_echo(t_mshell *mshell, char **args);
-void			ft_unset(t_env **env, char *key);
+void 			ft_unset(t_mshell *mshell, char **keys, int i, int index);
 t_env 			*get_env_var(t_env *env, char *key);
 void   			add_env_var(t_env **env, char *key, char *value);
-int				update_env_state(t_mshell *mshell, char **oldpwd, char **pwd);
+int				update_env_state(t_mshell *mshell, char *current_pwd);
 int 			cd_error(char **args, t_cd_error err);
 int 			builtins_execv(t_mshell *mshell, char *input_str);
 t_built_state 	*init_built_state();
@@ -50,4 +52,5 @@ int 			env_size(t_env *env);
 t_env  			*allocate_new_env(t_env *old_env);
 int  			ft_exit(t_mshell *mshell, char **args);
 int 			is_invalid_digit(char *arg);
+void			update_cd_env(t_env **env, const char *key, const char *set_path);
 #endif
