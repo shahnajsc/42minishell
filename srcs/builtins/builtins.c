222a2eeeb79325ed44d17b3fab4dbd8f6c9f03c7
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-08 14:54:48 +0200

    uncomment 'ft_unse't

--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -11,7 +11,7 @@ int builtins_execv(t_mshell *mshell, char *input_str)
 	if (ft_strcmp(args[0], "env") == 0)
 		ft_env(mshell, args);
 	else if (ft_strcmp(args[0], "pwd") == 0)
-		ft_pwd();
+		ft_pwd(mshell);
 	else if (ft_strcmp(args[0], "export") == 0)
 		ft_export(mshell, args);
 	else if (ft_strcmp(args[0], "cd") == 0)
@@ -20,11 +20,9 @@ int builtins_execv(t_mshell *mshell, char *input_str)
 		ft_echo(mshell, args);
 	else if (ft_strcmp(args[0], "exit") == 0)
 		ft_exit(mshell, args);
-	// else if (ft_strcmp(args[0], "unset") == 0)
-	// 	ft_unset(&mshell->env, args[1]);
+	else if (ft_strcmp(args[0], "unset") == 0)
+		ft_unset(mshell, args, 0, 0);
 	add_history(input_str);
 	rl_clear_history();
-	// free_env(mshell->env, env_size(&mshell->env));
 	return (EXIT_SUCCESS);
 }
-
