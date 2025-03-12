9c17021c8b27c38bac513153f13b401af016fad0
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-03-12 17:14:07 +0200

    Update: handled sigint and sigquit

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -1,10 +1,36 @@
 #include "minishell.h"
 
-void	setup_ignore_sigquit()
+void 	reset_prompt()
+{
+	ft_putchar_fd('\n', STDERR_FILENO);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void 	reset_sigint()
+{
+	struct sigaction sa;
+
+	ft_memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = reset_prompt;
+	sigemptyset(&sa.sa_mask); 
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		return ;
+}
+void  ignore_sigquit()
 {
 	struct sigaction sa;
 
 	ft_memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = SIG_IGN;
-	sigaction(SIGQUIT, &sa, NULL);
+	sigemptyset(&sa.sa_mask); 
+	if (sigaction(SIGQUIT, &sa, NULL) == -1 )
+		return ;
+}
+void  setup_signal_handlers()
+{
+	ignore_sigquit();
+
+	
 }
